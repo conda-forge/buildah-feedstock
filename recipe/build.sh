88adea7063ca77937f466f1fe3bfca7ad967eb53
Daniel Nachun <daniel.nachun@gmail.com>
2026-03-29 15:00:05 -0700

    try to clean up script

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,12 @@
 
 # Inject prefix path. (With sed since I couldn't get it to work with something
 # like EXTRA_LDFLAGS="-X something.condaPrefix=${PREFIX}" in make commands.)
-sed -i.bak \
-  's|^\(// const char \*condaPrefix =\).*|\1 "'"${PREFIX}"'";|' \
-  vendor/github.com/containers/common/pkg/config/config.go
-grep -qF \
-  "// const char *condaPrefix = \"${PREFIX}\";" \
-  vendor/github.com/containers/common/pkg/config/config.go
+# sed -i.bak \
+#   's|^\(// const char \*condaPrefix =\).*|\1 "'"${PREFIX}"'";|' \
+#   vendor/github.com/containers/common/pkg/config/config.go
+# grep -qF \
+#   "// const char *condaPrefix = \"${PREFIX}\";" \
+#   vendor/github.com/containers/common/pkg/config/config.go
 
 make \
   GIT_COMMIT= \
@@ -19,44 +19,46 @@ make \
   PREFIX="${PREFIX}" \
   install install.completions
 
+go-licenses save . --save_path=license-files
 
-gather_licenses() {
-  # shellcheck disable=SC2039  # Allow widely supported non-POSIX local keyword.
-  local module output tmp_dir acc_dir
-  output="${1}"
-  shift
-  tmp_dir="$(pwd)/gather-licenses-tmp"
-  acc_dir="$(pwd)/gather-licenses-acc"
-  mkdir "${acc_dir}"
-  cat > "${output}" <<'EOF'
---------------------------------------------------------------------------------
-The output below is generated with `go-licenses csv` and `go-licenses save`.
-================================================================================
-EOF
-  for module ; do
-    cat >> "${output}" <<EOF
+# gather_licenses() {
+#   # shellcheck disable=SC2039  # Allow widely supported non-POSIX local keyword.
+#   local module output tmp_dir acc_dir
+#   output="${1}"
+#   shift
+#   tmp_dir="$(pwd)/gather-licenses-tmp"
+#   acc_dir="$(pwd)/gather-licenses-acc"
+#   mkdir "${acc_dir}"
+#   cat > "${output}" <<'EOF'
+# --------------------------------------------------------------------------------
+# The output below is generated with `go-licenses csv` and `go-licenses save`.
+# ================================================================================
+# EOF
+#   for module ; do
+#     cat >> "${output}" <<EOF
+#
+# go-licenses csv ${module}
+# ================================================================================
+# EOF
+#     go get -d "${module}"
+#     chmod -R +rw "$( go env GOPATH )"
+#     go-licenses csv "${module}" | sort >> "${output}"
+#     go-licenses save "${module}" --save_path="${tmp_dir}"
+#     cp -r "${tmp_dir}"/* "${acc_dir}"/
+#     rm -r "${tmp_dir}"
+#   done
+#   # shellcheck disable=SC2016  # Not expanding $ in single quotes intentional.
+#   find "${acc_dir}" -type f | sort | xargs -L1 sh -c '
+# cat <<EOF
+#
+# --------------------------------------------------------------------------------
+# ${2#${1%/}/}
+# ================================================================================
+# EOF
+# cat "${2}"
+# ' -- "${acc_dir}" >> "${output}"
+#   rm -r "${acc_dir}"
+# }
+#
+# gather_licenses ./thirdparty-licenses.txt .
 
-go-licenses csv ${module}
-================================================================================
-EOF
-    go get -d "${module}"
-    chmod -R +rw "$( go env GOPATH )"
-    go-licenses csv "${module}" | sort >> "${output}"
-    go-licenses save "${module}" --save_path="${tmp_dir}"
-    cp -r "${tmp_dir}"/* "${acc_dir}"/
-    rm -r "${tmp_dir}"
-  done
-  # shellcheck disable=SC2016  # Not expanding $ in single quotes intentional.
-  find "${acc_dir}" -type f | sort | xargs -L1 sh -c '
-cat <<EOF
-
---------------------------------------------------------------------------------
-${2#${1%/}/}
-================================================================================
-EOF
-cat "${2}"
-' -- "${acc_dir}" >> "${output}"
-  rm -r "${acc_dir}"
-}
-
-gather_licenses ./thirdparty-licenses.txt .
